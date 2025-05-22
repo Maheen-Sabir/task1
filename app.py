040bf9138c04453263f69e13a555dc475e9e6998
Maheen <m.sabir2@studenti.unipi.it>
2025-05-22 13:16:14 +0200

    files added

---
+++ app.py
@@ -0,0 +1,52 @@
+from flask import Flask, request, make_response, jsonify
+
+app = Flask(__name__, instance_relative_config=True)
+
+@app.route('/add')
+def add():
+    a = request.args.get('a', type=float)
+    b = request.args.get('b', type=float)
+    if a and b:
+        return make_response(jsonify(s=a+b), 200) #HTTP 200 OK
+    else:
+        return make_response('Invalid input\n', 400) #HTTP 400 BAD REQUEST
+
+#Endpoint /sub for subtraction which takes a and b as query parameters.
+
+@app.route('/sub')
+def sub():
+    a = request.args.get('a', type=float)
+    b = request.args.get('b', type=float)
+    if a and b:
+        return make_response(jsonify(s=a-b), 200) #HTTP 200 OK
+    else:
+        return make_response('Invalid input\n', 400) #HTTP 400 BAD REQUEST
+#Endpoint /mul for multiplication which takes a and b as query parameters.
+
+@app.route('/mul')
+def mul():
+    a = request.args.get('a', type=float)
+    b = request.args.get('b', type=float)
+    if a and b:
+        return make_response(jsonify(s=a*b), 200) #HTTP 200 OK
+    else:
+        return make_response('Invalid input\n', 400) #HTTP 400 BAD REQUEST
+#Endpoint /div for division which takes a and b as query parameters. Returns HTTP 400 BAD REQUEST also for division by zero.
+
+@app.route('/div')
+def div():
+    a = request.args.get('a', type=float)
+    b = request.args.get('b', type=float)
+    if a and b:
+        return make_response(jsonify(s=a/b), 200) #HTTP 200 OK
+    elif b == 0:
+        # division by zero
+       return make_response('Invalid input\n', 400) #HTTP 400 BAD REQUEST
+    else:
+        return make_response('Invalid input\n', 400) #HTTP 400 BAD REQUEST
+#Endpoint /mod for modulo which takes a and b as query parameters. Returns HTTP 400 BAD REQUEST also for division by zero.
+
+#Endpoint /random which takes a and b as query parameters and returns a random number between a and b included. Returns HTTP 400 BAD REQUEST if a is greater than b.
+
+if __name__ == '__main__':
+    app.run(debug=True)
